f2f7215a01a63c92fb02e6bf43c5e45c73aa7355
King Celestial <139430697+KingCelestial@users.noreply.github.com>
2023-11-03 21:07:37 +0530

    Email_Application

---
+++ beginner_projects/Email.java
@@ -0,0 +1,26 @@
+package beginner_projects;
+
+public class Email {
+	private String firstname;
+	private String lastname;
+	private String password;
+	private String department;
+	private String alternateemail;
+	private int mailboxCapacity;
+	
+	// constructor to receive the first name and last name
+	public Email(String firstname, String lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		System.out.println("EMAIL CREATED: " + this.firstname +" "+ this.lastname);
+	}
+	// Ask for the department
+	
+	// generate a random password
+	
+	// set the mailbox capacity
+	
+	// set the alternate email
+	
+	// change the password
+}
--- beginner_projects/Email_Application.java
+++ beginner_projects/Email_Application.java
@@ -4,7 +4,7 @@ public class Email_Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Email em1 = new Email("John","Smith");
 	}
 
 }
